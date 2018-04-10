78f764caf22e58355768ea621a184717e4b56b44
Abarkevich <36437669+Abarkevich@users.noreply.github.com>
2018-04-10 18:03:50 +0300

    send demo

--- src/main/java/consensus/components/sendDemo/SendDemo.java
+++ src/main/java/consensus/components/sendDemo/SendDemo.java
@@ -2,10 +2,19 @@ package consensus.components.sendDemo;
 
 import com.codeborne.selenide.SelenideElement;
 import core.components.Component;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class SendDemo extends Component {
-    public static final SelenideElement SEND_DEMO_BTN = $("//a[@id='btn-send-a-demo']");
+    public static final SelenideElement SEND_DEMO_LEFT_MENU_BTN = $(By.xpath("//a[@id='btn-send-a-demo']"));
+    public static final SelenideElement ORGANIZATION_FIELD = $(By.xpath("//input[@id='s1_organization']"));
+    public static final SelenideElement RECIPIENT_MAIL_FIELD = $(By.xpath("//input[@id='s1_mail']"));
+    public static final SelenideElement RECIPIENT_FIRST_NAME_FIELD = $(By.xpath("//input[@id='s1_fname']"));
+    public static final SelenideElement RECIPIENT_LAST_NAME_FIELD = $(By.xpath("//input[@id='s1_lname']"));
+    public static final SelenideElement RECIPIENT_TITLE_FIELD = $(By.xpath("//input[@id='s1_title']"));
+    public static final SelenideElement ADD_NEW_RECIPIENT_BTN = $(By.xpath("//button[@class='btn primary new_recipient']")); //уточнить у Димы: кесли добавить +1 реципиент, то появляется 2-я кнопка ADD и тогда локатор становится не уникальным
+
+
 
 }
